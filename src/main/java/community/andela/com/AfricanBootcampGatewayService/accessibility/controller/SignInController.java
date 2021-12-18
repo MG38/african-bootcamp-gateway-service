7c555f3a99d3511e814e45241156ae58f86e1539
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-12-18 12:45:23 +0100

    fixing response header bug

--- src/main/java/community/andela/com/AfricanBootcampGatewayService/accessibility/controller/SignInController.java
+++ src/main/java/community/andela/com/AfricanBootcampGatewayService/accessibility/controller/SignInController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 
@@ -29,14 +31,14 @@ public class SignInController {
         return deferredResult;
     }
     @GetMapping("/login")
-    public DeferredResult<ResponseEntity> signIn(@RequestBody @Valid User user, BindingResult bindingResult){
+    public DeferredResult<ResponseEntity> signIn(@RequestBody @Valid User user, HttpServletResponse response,BindingResult bindingResult){
         var deferredResult = new DeferredResult<ResponseEntity>();
 
         if(!bindingResult.hasErrors()){
             signInService.signIn(user).subscribe(
                     token -> {
                         var responseEntity = new ResponseEntity(HttpStatus.OK);
-                        responseEntity.getHeaders().add("Token",token);
+                        response.setHeader("Token",token);
                         deferredResult.setResult(responseEntity);
                     },
                     error -> {
@@ -50,23 +52,23 @@ public class SignInController {
     }
 
     @GetMapping("/logout")
-    public DeferredResult<ResponseEntity> signOut(){
+    public DeferredResult<ResponseEntity> signOut(HttpServletResponse response){
         var responseEntity = new ResponseEntity(HttpStatus.OK);
         var deferredResult = new DeferredResult<ResponseEntity>();
-        responseEntity.getHeaders().add("Token","");
+        response.setHeader("Token","");
         deferredResult.setResult(responseEntity);
         return deferredResult;
     }
 
     @PostMapping("/accounts")
-    public DeferredResult<ResponseEntity<User>> createAccount(@RequestBody @Valid User user, BindingResult bindingResult){
+    public DeferredResult<ResponseEntity<User>> createAccount(@RequestBody @Valid User user, BindingResult bindingResult, HttpServletResponse response){
         var deferredResult = new DeferredResult<ResponseEntity<User>>();
 
         if(!bindingResult.hasErrors()){
             signInService.createAccount(user).subscribe(
                     createdUser -> {
                         var responseEntity = new ResponseEntity(createdUser,HttpStatus.OK);
-                        responseEntity.getHeaders().add("Token","");
+                        response.setHeader("Token", "");
                         deferredResult.setResult(responseEntity);
                     },
                     error -> {
